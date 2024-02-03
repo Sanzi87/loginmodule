9d0034141b586c6231c5abe266eac11c8d825199
Alexander Blomberg <info@dalasoftware.se>
2024-02-03 17:53:31 +0100

    Add background image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center'>
       <LoginModule />
+      <Image
+        className='-z-10 bg-center bg-cover opacity-80'
+        src='/purple_sky_with_stars_bg_1920.webp'
+        alt='Login background image'
+        fill={true}
+      />
     </main>
   );
 }
